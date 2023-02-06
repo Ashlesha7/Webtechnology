5d027ab2996668611adebe9495eb53a77e1409c3
Ashlesha KC <np03cs4a220157@heraldcollege.edu.np>
2023-02-06 12:54:54 +0545

    Added portfolio

---
+++ JavaScript/week9.js
@@ -0,0 +1,22 @@
+console.log('Hello World');
+//alert("hello Section 15");
+// prompt("What is your name?");
+
+let x=10;
+let y=20;
+console.log(x+y);
+const color="pink";
+console.log(color);
+console.log(x+y);
+console.log(x>y);
+let num="5";
+let number=5;
+console.log(num==number);
+
+let name="John"
+let Name="John"
+
+//Q1 Calculate how many seconds are there in 5 hour.
+let s=5;
+seconds=5*60*60;
+console.log(seconds);
---
+++ Week 9 & 10/wnine.js
@@ -0,0 +1,7 @@
+let country = 'Japan';
+let continent = 'Asia';
+let population = 10;
+
+console.log(country);
+console.log(continent);
+console.log(population);
